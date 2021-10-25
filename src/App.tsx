57d59796487923b36dd4e70e052f7ea7afdfb13e
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-10-25 14:44:04 -0300

    lista de fotos na home completa

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { useState, useEffect } from "react";
 import * as C from "./App.styles";
+import * as Photos from "./services/photo";
+import { Photo } from "./types/Photo";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    };
+    getPhotos();
+  }, []);
+
   return (
     <C.Container>
       <C.Area>
@@ -8,7 +22,20 @@ const App = () => {
 
         {/* Area de upload */}
 
-        {/* Lista de fotos */}
+        {loading && (
+          <C.ScreenWarning>
+            <div className="emoji">🤚</div>
+            <div>Carregando...</div>
+          </C.ScreenWarning>
+        )}
+
+        {!loading && photos.length > 0 && (
+          <C.PhotosList>
+            {photos.map((item, index) => (
+              <div>{item.name}</div>
+            ))}
+          </C.PhotosList>
+        )}
       </C.Area>
     </C.Container>
   );
